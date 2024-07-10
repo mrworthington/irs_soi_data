1a686d5216b0963933ca619d70182d26f5d7a211
Matt Worthington <worthington.matthew@gmail.com>
2024-07-10 09:12:09 -0500

    Update download_files.R

--- download_files.R
+++ download_files.R
@@ -7,25 +7,25 @@ library(nanoparquet)
 
 years <- c("2122","2021", "1920", "1819", "1718", "1617", "1516", "1415", "1314", "1213", "1112")
 
-# state_outflows_df <-
+state_outflows_df <-
   map(years,
       ~read_csv(paste0("https://www.irs.gov/pub/irs-soi/stateoutflow",.x,".csv")) |>
         mutate(year = .x) |>
         select(year, everything())) |>
   list_rbind() |>
   clean_names() |>
-  write_parquet("state_outflow_data.parquet")
+  write_parquet("clean_data/state_outflow_data.parquet")
 
-# state_inflows_df <-
+state_inflows_df <-
   map(years,
       ~read_csv(paste0("https://www.irs.gov/pub/irs-soi/stateinflow",.x,".csv")) |>
         mutate(year = .x) |>
         select(year, everything())) |>
   list_rbind() |>
   clean_names() |>
-  write_parquet("state_inflow_data.parquet")
+  write_parquet("clean_data/state_inflow_data.parquet")
 
-# county_outflows_df <-
+county_outflows_df <-
   map(years,
       ~read_csv(paste0("https://www.irs.gov/pub/irs-soi/countyoutflow",.x,".csv")) |>
         mutate(year = .x,
@@ -34,9 +34,9 @@ years <- c("2122","2021", "1920", "1819", "1718", "1617", "1516", "1415", "1314"
         select(year, everything())) |>
   list_rbind() |>
   clean_names() |>
-  write_parquet("county_outflow_data.parquet")
+  write_parquet("clean_data/county_outflow_data.parquet")
 
-# county_inflows_df <-
+county_inflows_df <-
   map(years,
       ~read_csv(paste0("https://www.irs.gov/pub/irs-soi/countyinflow",.x,".csv")) |>
         mutate(year = .x,
@@ -45,4 +45,4 @@ years <- c("2122","2021", "1920", "1819", "1718", "1617", "1516", "1415", "1314"
         select(year, everything())) |>
   list_rbind() |>
   clean_names() |>
-  write_parquet("county_inflow_data.parquet")
+  write_parquet("clean_data/county_inflow_data.parquet")
