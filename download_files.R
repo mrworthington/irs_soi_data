b372112f842a130f83e459d15127573c91f6d625
Matt Worthington <worthington.matthew@gmail.com>
2024-07-10 01:02:32 -0500

    Switch to smaller package set

--- download_files.R
+++ download_files.R
@@ -1,6 +1,8 @@
-library(tidyverse)
+library(readr)
+library(dplyr)
+library(purrr)
 library(janitor)
-library(arrow)
+library(nanoparquet)
 
 years <- c("2122","2021", "1920", "1819", "1718", "1617", "1516", "1415", "1314", "1213", "1112")
 
@@ -10,4 +12,34 @@ state_outflows_df <-
         mutate(year = .x) |>
         select(year, everything())) |>
   list_rbind() |>
-  clean_names()
+  clean_names() |>
+  write_parquet("clean_data/state_outflow_data.parquet")
+
+state_inflows_df <-
+  map(years,
+      ~read_csv(paste0("https://www.irs.gov/pub/irs-soi/stateinflow",.x,".csv")) |>
+        mutate(year = .x) |>
+        select(year, everything())) |>
+  list_rbind() |>
+  clean_names() |>
+  write_parquet("clean_data/state_inflow_data.parquet")
+
+county_outflows_df <-
+  map(years,
+      ~read_csv(paste0("https://www.irs.gov/pub/irs-soi/countyoutflow",.x,".csv")) |>
+        mutate(year = .x,
+               across(contains("fips"), ~as.character(.x))) |>
+        select(year, everything())) |>
+  list_rbind() |>
+  clean_names() |>
+  write_parquet("clean_data/county_outflow_data.parquet")
+
+county_inflows_df <-
+  map(years,
+      ~read_csv(paste0("https://www.irs.gov/pub/irs-soi/countyinflow",.x,".csv")) |>
+        mutate(year = .x,
+               across(contains("fips"), ~as.character(.x))) |>
+        select(year, everything())) |>
+  list_rbind() |>
+  clean_names() |>
+  write_parquet("clean_data/county_inflow_data.parquet")
